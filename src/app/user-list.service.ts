15d6f6395a3f4ba372ec4dba447fbcc2fec5e0de
DieDyeDai <mzdai@ad.unc.edu>
2024-02-25 01:00:23 -0500

    gitignore stuff

--- src/app/user-list.service.ts
+++ src/app/user-list.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { User } from './current-user.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ export class UserListService {
     {
       name: "user1",
       bio: "user1 bio",
-      interests: ["", ""],
-      email: "stuff",
+      interests: ["Aaaaa", "Bbbbb"],
+      email: "1@gmail.com",
       password: "things",
       gender: 0,
     },
@@ -22,32 +23,42 @@ export class UserListService {
     {
       name: "user2",
       bio: "user2 bio",
-      interests: ["", ""],
-      email: "stuff",
+      interests: ["Ccccc", "Ddddd"],
+      email: "2@gmail.com",
       password: "things",
-      gender: 0,
+      gender: 1,
     },
 
     {
-      name: "user2",
-      bio: "user2 bio",
-      interests: ["", ""],
-      email: "stuff",
+      name: "user3",
+      bio: "user3 bio",
+      interests: ["Eeeee", "Fffff"],
+      email: "3@gmail.com",
       password: "things",
-      gender: 0,
+      gender: 2,
     }
 
   ];
 
+  constructor(private http: HttpClient) {}
+
   private profiles: User[] = UserListService.dummyUsers;
   profilesUpdated = new Subject<void>();
-
-  addUserProfile(profile: User) {
-    this.profiles.push(profile);
-    this.profilesUpdated.next();
-  }
-
+  
   getUserProfiles() {
     return [...this.profiles];
+
+    this.http.get<User[]>("/api/users")
+    .subscribe((value) => {
+      
+    })
   }
+  
+  addUserProfile(profile: User): Observable<User> {
+    this.profiles.push(profile);
+    this.profilesUpdated.next();
+    return this.http.put<User>("/api/users", profile) as Observable<User>;
+  }
+
+
 }
